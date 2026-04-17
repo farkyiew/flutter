8a7bac7c7400dd0f063528478f3e6027f9b05784
farkyiew <72897967+farkyiew@users.noreply.github.com>
2026-04-17 08:19:02 +0800

    Refactor SnaraiSurah and Surah classes

--- snsurah.dart
+++ snsurah.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tafsir/config/loading.dart';
 import 'package:tafsir/design/design.dart';
-
 import 'package:tafsir/todb.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
